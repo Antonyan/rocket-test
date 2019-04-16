8e80786e6796634d4def39162e888647363422c9
Yuriy Stetsiv <ystetsiv@xlntech.co.uk>
2019-04-16 16:28:53 +0300

    [Update] change dependencies versions

--- src/main/java/core/modules/web/models/WebBrowser.java
+++ src/main/java/core/modules/web/models/WebBrowser.java
@@ -6,34 +6,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.logging.LogType;
-import org.openqa.selenium.logging.LoggingPreferences;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.safari.SafariDriver;
-
-import java.util.logging.Level;
 
 public class WebBrowser {
 
-    public static WebDriver initialize(String browser, DesiredCapabilities capabilities) {
-        synchronized (BrowserManager.class) {
+    public static WebDriver initialize(String browser) {
+         {
             switch (browser.toLowerCase()) {
                 case "chrome":
-                    ChromeDriverManager.getInstance().setup();
-                    DesiredCapabilities caps = DesiredCapabilities.chrome();
-                    LoggingPreferences logPrefs = new LoggingPreferences();
-                    logPrefs.enable(LogType.BROWSER, Level.ALL);
-                    caps.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
-                    return new ChromeDriver(caps);
+                    WebDriverManager.chromedriver().setup();
+                    return new ChromeDriver();
                 case "firefox":
-                    FirefoxDriverManager.getInstance().setup();
+                    WebDriverManager.firefoxdriver().setup();
                     return new FirefoxDriver();
                 case "ie":
-                    InternetExplorerDriverManager.getInstance().setup();
+                    WebDriverManager.iedriver().setup();
                     return new InternetExplorerDriver();
                 case "edge":
-                    EdgeDriverManager.getInstance().setup();
+                    WebDriverManager.edgedriver().setup();
                     return new EdgeDriver();
                 case "safari":
                     return new SafariDriver();
